9e35446df7ce6ab5c2e9e337d328f33be09c9e6e
Atlas <77736272+ASproson@users.noreply.github.com>
2021-11-01 14:41:01 +0000

    Passes first test when passed an unknown API path

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,7 @@
 const db = require('../db/connection.js');
 const request = require('supertest');
 const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const seed = require('../db/seeds/seed.js');
 const app = require('../app');
 
 beforeEach(() => seed(testData));
