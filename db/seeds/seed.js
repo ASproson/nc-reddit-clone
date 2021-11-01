d73573c2433d1e5cafd28eb604f10d45eb61ba5f
Atlas <77736272+ASproson@users.noreply.github.com>
2021-11-01 10:57:21 +0000

    Adds DROP TABLE IF EXISTS protections to start of seed

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -5,6 +5,14 @@ const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
   // 1. create tables
   // 2. insert data
+  return db
+  .query(`DROP TABLE IF EXISTS topicData;`)
+  .then(() => {
+    return db.query(`DROP TABLE IF EXISTS articleData;`)})
+  .then(() => { 
+    return db.query(`DROP TABLE IF EXISTS userData;`)})
+  .then(() => {
+    return db.query(`DROP TABLE IF EXISTS commentData;`)})
   
 };
 
