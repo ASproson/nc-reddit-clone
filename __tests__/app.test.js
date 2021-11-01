5ce6c6835ccf201f736faf77d527ca634229b063
Atlas <77736272+ASproson@users.noreply.github.com>
2021-11-01 14:30:17 +0000

    Requires in various files such as app.js, and supertest

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,8 @@
 const db = require('../db/connection.js');
+const request = require('supertest');
 const testData = require('../db/data/test-data/index.js');
 const { seed } = require('../db/seeds/seed.js');
+const app = require('../app');
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
