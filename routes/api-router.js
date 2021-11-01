b04c2ab01b7d2dd46100bbf11ca5421028984e11
Atlas <77736272+ASproson@users.noreply.github.com>
2021-11-01 15:15:46 +0000

    Adds articles router and sets articles path

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,6 +1,8 @@
 const apiRouter = require('express').Router();
 const topicsRouter = require('../routes/topics-router.js');
+const articlesRouter = require('../routes/articles-router.js');
 
 apiRouter.use('/topics', topicsRouter);
+apiRouter.use('/articles', articlesRouter);
 
 module.exports = apiRouter;
