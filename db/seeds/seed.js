dd15b5bf7e6daf31c6765d0d9dfc9a09b7eb3d43
Atlas <77736272+ASproson@users.noreply.github.com>
2021-11-01 12:04:06 +0000

    Updates packages with pg-format

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,18 +1,11 @@
 const db = require('../connection');
-// const format = require('pg-format');
+const format = require('pg-format');
 
 const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
   // 1. create tables
-  // 2. insert data
   return db
-  .query(`DROP TABLE IF EXISTS articleData;`)
-  .then(() => {
-    return db.query(`DROP TABLE IF EXISTS commentData;`)})
-  .then(() => { 
-    return db.query(`DROP TABLE IF EXISTS userData;`)})
-  .then(() => {
-    return db.query(`DROP TABLE IF EXISTS topicData;`)})
+  .query(`DROP TABLE IF EXISTS articleData, commentData, topicData, userData;`)
   .then(() => {
     return db
     .query(`
@@ -34,13 +27,13 @@ const seed = (data) => {
     return db
     .query(`
       CREATE TABLE articleData (
-        article_ID SERIAL PRIMARY KEY,
-        title VARCHAR(50) NOT NULL,
-        body VARCHAR(255) NOT NULL,
-        votes INT DEFAULT 0,
-        topic VARCHAR(50) REFERENCES topicData(slug),
-        author VARCHAR(50) REFERENCES userData(username),
-        created_at DATE DEFAULT CURRENT_TIMESTAMP
+        article_ID  SERIAL PRIMARY KEY,
+        title       VARCHAR(50) NOT NULL,
+        body        VARCHAR(255) NOT NULL,
+        votes       INT DEFAULT 0,
+        topic       VARCHAR(50) REFERENCES topicData(slug),
+        author      VARCHAR(50) REFERENCES userData(username),
+        created_at  DATE DEFAULT CURRENT_TIMESTAMP
       );`)
   })
   .then(() => {
@@ -54,6 +47,12 @@ const seed = (data) => {
         created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
         body        VARCHAR(500) NOT NULL
     );`)
+  })
+  // 2. insert data
+  .then(() => {
+    const sqlInsert = format(`INSERT INTO topicData (slug, description) VALUES %L RETURNING *;`);
+    topicData.map((topic) => { return [topic.slug, topic.description] });
+    return db.query(sqlInsert)
   })
 };
 
