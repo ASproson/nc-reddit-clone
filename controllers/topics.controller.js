58012423f274c2acca7e9aeaefc5c6003667b49e
Atlas <77736272+ASproson@users.noreply.github.com>
2021-11-01 14:53:18 +0000

    Updates topics.controller to hit topics.model and return status:200, data

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,5 +1,10 @@
 const { selectTopics } = require('../models/topics.model');
 
 exports.getTopics = (req, res, next) => {
-    
+    const { description, slug } = req.query;
+    selectTopics(description, slug)
+    .then((topics) => {
+        res.status(200).send({topics});
+    })
+    .catch(next);
 }
