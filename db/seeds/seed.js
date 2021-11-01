fd1727ea5b0ba3f8505d6d28fc075d1f4b2f10d4
Atlas <77736272+ASproson@users.noreply.github.com>
2021-11-01 10:48:47 +0000

    Brings in pg-format and creates the db link to Pool

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,11 @@
+const db = require('../connection');
+const format = require('pg-format');
+
 const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
   // 1. create tables
   // 2. insert data
+  
 };
 
 module.exports = seed;
