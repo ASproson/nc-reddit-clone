f949c59b4487b0619d04669fefed5b350262f034
Atlas <77736272+ASproson@users.noreply.github.com>
2021-11-02 11:46:58 +0000

    comments out unneeded PSQL message for the mean time

--- errors.js
+++ errors.js
@@ -7,15 +7,16 @@ exports.handleCustoms = (err, req, res, next) => {
 }
 
 exports.handlePSQLErrors = (err, req, res, next) => {
+    console.log('Inside handlePSQLErrors')
     if(err.code === '22P02'){
         res.status(400).send({msg: 'invalid data type' });
     } else {
-        const { status, msg} = errorCodes[err.code]
+        // const { status, msg } = errorCodes[err.code]
         res.status(status).send({ msg });
     }
 }
 
 exports.handle500s = (err, req, res, next) => {
-    console.log(err);
+    console.log('Inside handle 500s', err);
     res.status(500).send({msg: 'server error'});
 }
