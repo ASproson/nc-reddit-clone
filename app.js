ea6c599b454cbd59f2eb0b20d7ae48022dbebd8e
Atlas <77736272+ASproson@users.noreply.github.com>
2021-11-02 09:19:07 +0000

    Adds express.json() and reorganizes error handling to trigger in the correct order

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const app = require('express')();
+const express = require('express');
 const apiRouter = require('./routes/api-router');
-const handlePSQLErrors = require('./errors');
+const { handle500s, handleCustoms, handlePSQLErrors } = require('./errors.js')
+
+app.use(express.json());
 
 app.use('/api', apiRouter);
 
 app.all('/*', (req, res) => {res.status(404).send({msg: 'path not found'})});
 
+app.use(handleCustoms);
 app.use(handlePSQLErrors);
+app.use(handle500s);
 
 app.use(function(err, req, res, next) {
-    console.log(err);
+    console.log('App.JS Error log', err);
     res.status(500).send({msg: 'internal server error'});
 });
-
 
 module.exports = app;
