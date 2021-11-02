472335e46f772c380d3cb97eead48c51d49a942f
Atlas <77736272+ASproson@users.noreply.github.com>
2021-11-02 12:43:24 +0000

    Adds patchVotes to the articles controller and correctly collects body from test patch-send request

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,10 +1,20 @@
-const { selectArticles } = require('../models/articles.model');
+const { selectArticles, updateArticleVotesById } = require('../models/articles.model');
 
 exports.getArticles = (req, res, next) => {
     const { article_id: id } = req.params;
     selectArticles(id)
     .then((article) => {
         res.status(200).send({article});
+    })
+    .catch(next);
+}
+
+exports.patchVotes = (req, res, next) => {
+    const { article_id: id } = req.params;
+    const { inc_votes } = req.body;
+    updateArticleVotesById(id, inc_votes)
+    .then((article) => {
+        res.status(201).send({ article })
     })
     .catch(next);
 }
