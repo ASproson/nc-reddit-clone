dc1d2072c1a2670aef2eb95f4b7807a53ead8348
Atlas <77736272+ASproson@users.noreply.github.com>
2021-11-02 08:43:41 +0000

    Invokes the use of handlePSQLerrors to handle invalid data type requests

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const app = require('express')();
 const apiRouter = require('./routes/api-router');
+const handlePSQLErrors = require('./errors');
 
 app.use('/api', apiRouter);
 
 app.all('/*', (req, res) => {res.status(404).send({msg: 'path not found'})});
 
+app.use(handlePSQLErrors);
+
 app.use(function(err, req, res, next) {
     console.log(err);
     res.status(500).send({msg: 'internal server error'});
 });
+
 
 module.exports = app;
