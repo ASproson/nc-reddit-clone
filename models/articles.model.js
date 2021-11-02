4f5c76d4f92314527597d7942fc8b9d603df10d9
Atlas <77736272+ASproson@users.noreply.github.com>
2021-11-02 11:45:50 +0000

    refactors SQL query to be DRY and more readable

--- models/articles.model.js
+++ models/articles.model.js
@@ -2,15 +2,29 @@ const db = require('../db/connection.js');
 
 exports.selectArticles = (id) => {
 
-    const queryStr = `
-        SELECT 
-            articles.*, 
-            COUNT(c.author) AS comment_count 
-        FROM articles 
+    let articleQuery = `SELECT articles.*, COUNT(c.author) AS comment_count FROM articles`;
+    const queryValues = [];
+    if(id){
+        articleQuery += ` 
         LEFT OUTER JOIN comments AS c ON articles.article_id = c.article_id 
-        WHERE articles.article_id = $1 GROUP BY articles.article_id
-    `
+        WHERE articles.article_id = $1 GROUP BY articles.article_id;
+        `;
+        queryValues.push(id);
+    }
 
-    return db.query(queryStr, [id])
+    return db.query(articleQuery, queryValues)
+
+    // const queryStr = `
+    //     SELECT 
+    //         articles.*, 
+    //         COUNT(c.author) AS comment_count 
+    //     FROM articles 
+    //     LEFT OUTER JOIN comments AS c ON articles.article_id = c.article_id 
+    //     WHERE articles.article_id = $1 GROUP BY articles.article_id
+    // `;
+    // return db.query(queryStr, [id])
+
+
+
     .then(({ rows }) => rows[0]);
 }
